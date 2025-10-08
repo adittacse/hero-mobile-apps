00383a8f85b0195cf426b95a2c28cba4838e17d0
Aditta Chakraborty <cseaditta@gmail.com>
2025-10-09 03:27:33 +0600

    loading component design improved with animation

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import logo from "../../assets/logo.png";
 
 const Loading = () => {
     return (
         <div className="flex flex-col justify-center items-center min-h-screen">
-            <span className="loading loading-spinner loading-xl"></span>
+            <h1 className="flex items-center font-bold text-5xl text-[#627382]">
+                L<img className="w-20 h-20 mx-6 animate-[spin_1.1s_linear_infinite]" src={logo} alt="loading logo" />ading
+            </h1>
         </div>
     );
 };
